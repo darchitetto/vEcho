d8bf8e1c55d1149ec092e315cad3f68742ff24bb
darchitetto <denise@digital-pioneers.com>
2016-02-01 14:22:40 -0500

    hardcode the response for now

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,29 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    var response = {
+                        "version": "1.0",
+                        "response": {
+                            "outputSpeech": {
+                                "type": "PlainText",
+                                "text": "Welcome to VersionOne vEcho"
+                            },
+                            "card": {
+                                "type": "Simple",
+                                "content": "Welcome to VersionOne vEcho",
+                                "title": "VersionOne vEcho"
+                            },
+                            "reprompt": {
+                                "outputSpeech": {
+                                    "type": "PlainText",
+                                    "text": "Welcome to VersionOne vEcho"
+                                }
+                            },
+                            "shouldEndSession": false
+                        },
+                        "sessionAttributes": null
+                    };
+    res.send(response);
 });
 
 var port = process.env.PORT || 3000;
@@ -10,157 +32,3 @@ var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
-
-
-exports.handler = function (event, context) {
-    try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
-
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-
-//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
-//         context.fail("Invalid Application ID");
-//      }
-
-        if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
-
-        if (event.request.type === "LaunchRequest") {
-            onLaunch(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "SessionEndedRequest") {
-            onSessionEnded(event.request, event.session);
-            context.succeed();
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
-    }
-};
-
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // add any session init logic here
-}
-
-function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    getWelcomeResponse(callback);
-}
-
-function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
-
-    // dispatch custom intents to handlers here
-    if ("AnswerIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("AnswerOnlyIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("AMAZON.StartOverIntent" === intentName) {
-        getWelcomeResponse(callback);
-    } else if ("AMAZON.RepeatIntent" === intentName) {
-        handleRepeatRequest(intent, session, callback);
-    } else if ("AMAZON.HelpIntent" === intentName) {
-        handleGetHelpRequest(intent, session, callback);
-    } else if ("AMAZON.StopIntent" === intentName) {
-        handleFinishSessionRequest(intent, session, callback);
-    } else if ("AMAZON.CancelIntent" === intentName) {
-        handleFinishSessionRequest(intent, session, callback);
-    } else {
-        throw "Invalid intent";
-    }
-}
-
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // Add any cleanup logic here
-}
-
-
-// ------- Skill specific business logic -------
-
-function getWelcomeResponse(callback) {
-    var cardTitle = "VersionOne v-echo",
-        speechOutput = "Welcome to Version One v-echo. I will read you",
-        shouldEndSession = false,
-        repromptText = " I will read you a story",
-        storyDescription = getStoryDescription("s-123456");
-
-    for (var i = 0; i < ANSWER_COUNT; i++) {
-        repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". "
-    }
-
-    speechOutput += repromptText;
-
-    var sessionAttributes = {
-        "speechOutput": repromptText,
-        "repromptText": repromptText,
-        "requestType": "read",
-        "storyDescription": storyDescription
-    };
-
-    callback(sessionAttributes,
-        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-function getStoryDescription(assetToken) {
-   //set token
-   //call API
-    return "This would be my description of an asset";
-}
-
-// ------- Helper functions to build responses -------
-
-
-function buildSpeechletResponse(title, speechOutput, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: speechOutput
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: speechOutput
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
-
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
-}
-
-
