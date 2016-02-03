8fd99b434a30744b4cfecad7cf18ffbca03aa20e
darchitetto <denise@digital-pioneers.com>
2016-02-03 15:50:50 -0500

    tets

--- scripts/index.js
+++ scripts/index.js
@@ -21,6 +21,7 @@ app.listen(port, function () {
 });
 
 function routeIntent(request){
+	console.log('REQUEST:::', request)
 	var intent = request.intent.name == undefined ? "" : request.intent.name;
 	var assetType = request.intent.slots.Asset.name == undefined ? "" : request.intent.slots.Asset.value;
 	var assetId = request.intent.slots.AssetNumber.value == undefined ? "" :request.intent.slots.AssetNumber.value;
