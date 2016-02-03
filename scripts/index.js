0ba957c12ec8140d35b0b6e76c8e9b5cf3313ddd
darchitetto <denise@digital-pioneers.com>
2016-02-03 12:14:23 -0500

    added everything to create an asset detail response and send it as a response

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,20 @@
 var bodyParser = require('body-parser')
 var express = require('express');
+var request = require('superagent');
+var Promise = require('promise');
+var agent = require('superagent-promise')(require('superagent'), Promise);
+
+
 var app = express();
 app.use(bodyParser.json())
 
 app.use(function (req, res) {
-	 //console.log("Type: " + req.body.request.type);
-	 //console.log("Intent: " + req.body.request.intent.name);
-	 //console.log("asset: " + req.body.request.intent.slots.Asset.name);
-	 //console.log("Asset Value: " + req.body.request.intent.slots.Asset.value);
-	 //console.log("Asset Number: " + req.body.request.intent.slots.AssetNumber.value);
-
-    routeIntent(req.body.request);
-
-	var response = {
-			"version": "1.0",
-				"response": {
-					"outputSpeech": {
-						"type": "PlainText",
-						"text": "Welcome to VersionOne vEcho"
-					},
-					"card": {
-						"type": "Simple",
-						"content": "Welcome to VersionOne vEcho",
-						"title": "VersionOne vEcho"
-					},
-					"reprompt": {
-						"outputSpeech": {
-							"type": "PlainText",
-							"text": "Welcome to VersionOne vEcho"
-						}
-					},
-					"shouldEndSession": false
-				},
-				"sessionAttributes": null
-			};
-		res.send(response);
+	var response = routeIntent(req.body.request);
+	response.then (function(result, err){
+		console.log (result);
+		return res.send(result);
 	});
+});
 
 var port = process.env.PORT || 3000;
 
@@ -46,15 +25,19 @@ app.listen(port, function () {
 function routeIntent(request){
 	var intent = request.intent.name == undefined ? "" : request.intent.name;
 	var assetType = request.intent.slots.Asset.name == undefined ? "" : request.intent.slots.Asset.value;
-	var assetId = request.intent.slots.AssetNumber;
+	var assetId = request.intent.slots.AssetNumber.value == undefined ? "" :request.intent.slots.AssetNumber.value;
+	var sentence, title, shouldEndSession;
+
 
 	switch(intent) {
 		case "Tell":{
-			console.log("Tell");
-			console.log("intent: " + intent);
-			console.log("assetType: " + assetType);
-			console.log("assetID: " + assetId);
-
+			title = "Version One Asset Details";
+			shouldEndSession = true;
+			return getAssetDetails(assetType, assetId)
+					.then(function(res,err){
+						sentence = createAssetDetailsResponse(JSON.parse(res.text),assetType);
+						return createResponse(title, sentence, shouldEndSession)
+				})
 		}
 		break;
 		case "Create":{
@@ -67,6 +50,85 @@ function routeIntent(request){
 	}
 }
 
-function getAssetDetails(){
+function getAssetDetails(assetType, assetId){
+	var url  = "http://builds.versionone.net/PR_16.0.2.11614/rest-1.v1/Data/" + assetType + '/' + assetId + '?sel=Name,Number,Scope.Name,Description,Status.Name,Priority.Name,Owners.Name,Estimate'
+
+	return agent('GET', url)
+		.set('Accept', 'application/json')
+		.end();
 
 }
+
+function createAssetDetailsResponse(assetDetails, assetType){
+	var attr = assetDetails.Attributes;
+
+	var details = {
+		'number': attr.Number.value,
+		'name': attr.Name.value,
+		'description': attr.Description.value,
+		'owner': attr["Owners.Name"].value[0],
+		'estimate': attr.Estimate.value,
+		'priority': attr["Priority.Name"].value,
+		'project': attr["Scope.Name"].value,
+		'status': attr["Status.Name"].value
+	}
+
+	var sentence = assetType + " " + details.number + " is named " + details.name + " and has a description of " + details.description + ".";
+	sentence += " It is owned by " + details.owner;
+	sentence += " and has an estimate of " + details.estimate;
+	sentence += " This " + assetType + "is of " + details.priority + " and has a status of " + details.status;
+	sentence += " It is being worked in the " + details.project + " project.";
+ 	console.log(sentence);
+	return sentence;
+}
+
+function createResponse(title, sentence, shouldEndsession){
+	return response = {
+		"version": "1.0",
+		"response": {
+			"outputSpeech": {
+				"type": "PlainText",
+				"text": sentence
+			},
+			"card": {
+				"type": "Simple",
+				"content": sentence,
+				"title": title
+			},
+			"reprompt": {
+				"outputSpeech": {
+					"type": "PlainText",
+					"text": sentence
+				}
+			},
+			"shouldEndSession": false
+		},
+		"sessionAttributes": null
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
